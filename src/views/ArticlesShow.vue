dd52af06a1b734af44905a0c3d949c1d8d58fdac
joe <joe.ulica@gmail.com>
2023-06-20 16:15:21 -0400

    add delete and update to showArticle page

--- src/views/ArticlesShow.vue
+++ src/views/ArticlesShow.vue
@@ -17,6 +17,27 @@ export default {
         console.log(response.data)
         this.article = response.data
       })
+    },
+    confirmDelete() {
+      document.querySelector("#confirmDelete").showModal()
+    },
+    deleteArticle() {
+      console.log("deleting article")
+      axios.delete(`http://localhost:3000/articles/${this.article.id}.json`).then(response => {
+        console.log(response.data)
+        this.$router.push("/articles")
+      })
+    },
+    showUpdate() {
+      document.querySelector("#showUpdate").showModal()
+    },
+    updateArticle() {
+      console.log("updating Article")
+      let article = { title: document.querySelector("#newTitle").value, text: document.querySelector("#newText").value }
+      axios.patch(`http://localhost:3000/articles/${this.article.id}.json`, article).then(response => {
+        console.log(response.data)
+        this.article = response.data
+      })
     }
   },
 };
@@ -30,6 +51,23 @@ export default {
       <img :src="photo.url">
     </div>
     <p> {{ article.text }}</p>
+    <button v-on:click="confirmDelete">delete</button>
+    <dialog id="confirmDelete">
+      <form method="dialog">
+        <p>Are you sure you want to delete?</p>
+        <button v-on:click="deleteArticle">Yes, remove it!</button>
+        <button>No, don't delete my article!</button>
+      </form>
+    </dialog>
+    <button v-on:click="showUpdate">update</button>
+    <dialog id="showUpdate">
+      <form method="dialog">
+        <label>Title: </label><input id="newTitle" type="text" :value="article.title"><br />
+        <label>text: </label><input id="newText" type="text" :value="article.text"><br />
+        <button v-on:click="updateArticle">Accept Changes</button>
+        <button>Reject Changes</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
