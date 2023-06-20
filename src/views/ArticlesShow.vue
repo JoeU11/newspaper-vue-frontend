00d71be26275320c069c27974ff285d1ccd8cb1a
joe <joe.ulica@gmail.com>
2023-06-20 12:41:23 -0400

    add show page for articles

---
+++ src/views/ArticlesShow.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () {
+    return {
+      article: {}
+    };
+  },
+  created: function () {
+    this.getArticle()
+  },
+  methods: {
+    getArticle() {
+      console.log("getting articles")
+      console.log(this.$route)
+      axios.get(`http://localhost:3000/articles/${this.$route.params.id}.json`).then(response => {
+        console.log(response.data)
+        this.article = response.data
+      })
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h2>{{ article.title }}</h2>
+    <h3>{{ article.author }}</h3>
+    <div v-for="photo in article.photos">
+      <img :src="photo.url">
+    </div>
+    <p> {{ article.text }}</p>
+  </div>
+</template>
+
+<style></style>
