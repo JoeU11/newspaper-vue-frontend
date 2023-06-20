b8a8ca4cd0e145eb686ea26cf4b3ad774ae2b17b
joe <joe.ulica@gmail.com>
2023-06-20 12:30:53 -0400

    add articles index page

---
+++ src/views/ArticlesIndex.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () {
+    return {
+      articles: []
+    };
+  },
+  created: function () {
+    this.getArticles()
+  },
+  methods: {
+    getArticles() {
+      console.log("getting articles")
+      axios.get("http://localhost:3000/articles.json").then(response => {
+        console.log(response.data)
+        this.articles = response.data
+      })
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <div v-for="article in articles">
+      <h2>{{ article.title }}</h2>
+      <h3>{{ article.author }}</h3>
+      <div v-for="photo in article.photos">
+        <img :src="photo.url">
+      </div>
+      <p> {{ article.text }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
